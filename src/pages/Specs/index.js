d641117f74594bb60ae6aa0f6f6b97490ad10e5a
Yuri <yurioliveira@aluno.fapce.edu.br>
2020-07-22 20:33:34 -0300

    style: Grafico de desempenho

--- src/pages/Specs/index.js
+++ src/pages/Specs/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Specifications, Title, Content, Label, Value, Skills, CarVideo } from './styles';
 
 export default function Specs(){
+
+  const v = 9.1;
+
   return(
     <Container>
-      <Text style={{ color: '#fff' }}>Especificações dos carros</Text>
+      <CarVideo
+        source={{ uri: 'https://live.staticflickr.com/video/41331881900/5d808122cb/1080p.mp4?s=eyJpIjo0MTMzMTg4MTkwMCwiZSI6MTU5NTQ2MTI1MywicyI6ImJjMTE4NjNjN2NkMDFkMWQ2OTBiMzE1OGI4OTA1YzQ1ZDJiYjE2YjgiLCJ2IjoxfQ' }}
+        rate={1.0}
+        isMuted
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+      />
+      <Specifications>
+      <Title>Desempenho</Title>
+      <View>
+        <Label>Velocidade</Label>
+        <Content style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Skills>
+            <Value velocity={v*10} />
+          </Skills>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>9,1</Text>
+        </Content>
+      </View>
+      <View>
+        <Label>Controle</Label>
+        <Content style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Skills>
+            <Value velocity={v*10} />
+          </Skills>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>9,1</Text>
+        </Content>
+      </View>
+      <View>
+        <Label>Aceleração</Label>
+        <Content style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Skills>
+            <Value velocity={v*10} />
+          </Skills>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>9,1</Text>
+        </Content>
+      </View>
+      <View>
+        <Label>Arrancada</Label>
+        <Content style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Skills>
+            <Value velocity={v*10} />
+          </Skills>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>9,1</Text>
+        </Content>
+      </View>
+      <View>
+        <Label>Frenagem</Label>
+        <Content style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Skills>
+            <Value velocity={v*10} />
+          </Skills>
+          <Text style={{color: '#fff', fontWeight: 'bold'}}>9,1</Text>
+        </Content>
+      </View>
+
+      </Specifications>
     </Container>
   );
 }
--- src/pages/Specs/styles.js
+++ src/pages/Specs/styles.js
@@ -1,8 +1,44 @@
 import styled from 'styled-components/native';
+import { Video } from 'expo-av';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background: #000;
+`;
+export const Specifications = styled.ScrollView`
+	padding: 15px;
+`;
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 22px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+export const CarVideo = styled(Video)`
+  width: 100%;
+  height: 200px;
+`;
+export const Content = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const Label = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  font-weight: bold;
+  margin-top: 7px;
+`;
+export const Skills = styled.View`
+  width: 300px;
+  height: 12px;
+  border-radius: 6px;
+
+  background: rgba(255, 255, 255, 0.5);
+  margin-right: 10px;
+`;
+export const Value = styled.View`
+  background: #d61e1e;
+  width: ${props => props.velocity}%;
+  height: 12px;
+  border-radius: 6px;
 `;
